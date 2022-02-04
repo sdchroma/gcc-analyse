6cc052455474b187e281062eb902838b9dff7c8a
sdchroma <47338000+sdchroma@users.noreply.github.com>
2022-02-04 14:16:46 +0300

    Update demo.c

--- demo.c
+++ demo.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
-
-int main(int argv, char **argc)
+int main()
 {
-    printf("hello world");
+    int i, n, t1 = 0, t2 = 1, nextTerm;
+    printf("Terim sayısını girin: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Dizisi : ");
+
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%d, ", t1);
+        nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    return 0;
 }
