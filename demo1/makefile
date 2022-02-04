87618450545be4496bbecc5850093b47ded0b05c
sdchroma <47338000+sdchroma@users.noreply.github.com>
2022-02-04 14:26:55 +0300

    Add files via upload

---
+++ demo1/makefile
@@ -0,0 +1,5 @@
+start: demo run
+demo: demo.c 
+	gcc -o demo demo.c
+run:
+	./demo
---
+++ demo2/demo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argv, char **argc)
+{
+    __uint16_t a[10];
+
+    for (int i = 0; i < 11; i++)
+    {
+        a[i] = i;
+    }
+}
