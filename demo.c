b9702a019ee53510ac0152d660fe76d713810a20
sdchroma <47338000+sdchroma@users.noreply.github.com>
2022-02-04 15:28:26 +0300

    Update demo.c

--- demo.c
+++ demo.c
@@ -2,12 +2,10 @@
 int main()
 {
     int i, n, t1 = 0, t2 = 1, nextTerm;
-    printf("Terim sayısını girin: ");
-    scanf("%d", &n);
 
     printf("Fibonacci Dizisi : ");
 
-    for (i = 1; i <= n; ++i)
+    for (i = 1; i <= 10; ++i)
     {
         printf("%d, ", t1);
         nextTerm = t1 + t2;
